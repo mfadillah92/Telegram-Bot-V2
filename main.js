72f580dd2e5fe87c980efc7c591f98285be6d074
M. Fadillah <163243265+mfadillah92@users.noreply.github.com>
2024-03-18 00:58:46 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,16 @@
 require("dotenv").config()
-const Bot = require("./app/Sefirosbot", "grammy")
-const { webhookCallback } = require("grammy")
-const express = require("express")
+const Sefirosbot = require("./app/Sefirosbot")
 
 const token = process.env.BOT_TOKEN
 const options = {
     polling: true
 }
 
-console.log("starting Sefirosbot...")
-const bot = new Bot(token, options)
+console.log("starting cuybot...")
+const bot = new Sefirosbot(token, options)
 
 const main = () => {
-	console.log("checking feature...")
+    console.log("checking feature...")
     bot.getHelp()
     bot.getSticker()
     bot.getGreeting()
@@ -22,19 +20,5 @@ const main = () => {
     bot.getQuake()
     console.log('feature ready!')
 }
-
-if (process.env.NODE_ENV === "production") {
-	const app = express()
-	app.use(express.json())
-	app.use(webhookCallback(bot, "express"))
-	
-	const PORT = process.env.PORT || 3000
-	app.listen(PORT, () => {
-		console.log(`Bot listening on port ${PORT}`)
-  })
-} else {
-	main()
-}
-
-process.once("SIGINT", () => main("SIGINT"))
-process.once("SIGTERM", () => main("SIGTERM"))
+main()
+console.log("Bot is ready now!")
