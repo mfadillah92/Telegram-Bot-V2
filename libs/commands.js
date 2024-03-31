d35a4a74d1da59c677469ce46b7b81e4afa18733
M. Fadillah <163243265+mfadillah92@users.noreply.github.com>
2024-03-31 21:32:26 +0700

    Update commands.js

--- libs/commands.js
+++ libs/commands.js
@@ -1,5 +1,6 @@
 const commands = {
     quote: /^!quote$/,
+	joke: /^!joke$/,
     news: /^!news$/,
     greeting: /^!halo$/,
     quake: /^!quake$/,
@@ -8,4 +9,3 @@ const commands = {
 }
 
 module.exports = commands
-
